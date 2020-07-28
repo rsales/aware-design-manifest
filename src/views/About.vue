de8ebf5e47876d4707f375191a8412c130c9c73c
Rafael Sales <rafael.sales93@hotmail.com>
2020-07-28 09:01:04 -0300

    Include tailwind in project.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="pt-16">
+    <img
+      class="object-cover h-64  w-full"
+      src="https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+    />
   </div>
 </template>
