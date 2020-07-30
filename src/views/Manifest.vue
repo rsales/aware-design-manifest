f43a25098a4c97885a44bc5c7dbfce354850b5df
Rafael Sales <rafael.sales93@hotmail.com>
2020-07-30 00:24:10 -0300

    Organize file 'manifest' and 'method'

---
+++ src/views/Manifest.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="manifest">
+    <div class="PageHeader PageHeader--tertiary">
+      <div class="Grid">
+        <div class="PageHeader-Content">
+          <h1 class="PageHeader-Title">
+            Connecting Teams: Um guia para uma cultura mais próxima, inovadora e
+            sustentável.
+          </h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="InlineContent">
+      <div class="TextBlock TextBlock--wide">
+        <div class="Grid">
+          <div class="TextBlock-Content">
+            <div class="Text TextStyles">
+              <p>
+                Apesar de vivermos na era digital, o MethodKit para Desenvolver
+                Equipes é uma ferramenta analógica e digital. Ele foi
+                desenvolvida para ajudar a estruturar e organizar o trabalho em
+                equipe, de maneira a ter mais eficácia, criatividade e
+                resultados. O kit, composto por vários cartões, pode ser usado
+                de diversas maneiras – num workshop de inauguração de projeto,
+                em reflexões com a equipe, sessões de coaching e muito mais.
+                Esta ferramenta reúne o essencial para ter e elaborar ideias,
+                servindo de base para fomentar a criatividade. A criação fica
+                por sua conta.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatum doloribus exercitationem consequuntur inventore
+                facilis nisi. Saepe reiciendis quasi impedit cum, assumenda,
+                nesciunt labore tempore qui nobis deserunt est maxime autem.
+                Voluptatum doloribus exercitationem consequuntur inventore
+                facilis nisi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="VideoBlock VideoBlock--inline VideoBlock--wide">
+        <div class="VideoBlock-ParagraphContainer"><span>&nbsp;</span></div>
+        <div class="VideoBlock-VideoContainer">
+          <div class="VideoBlock-Video">
+            <div style="width:100%;height:100%">
+              <div style="width:100%;height:100%">
+                <iframe
+                  frameborder="0"
+                  allowfullscreen="1"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  title="YouTube video player"
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/a9HjbSAK1qs?autoplay=0&amp;mute=0&amp;controls=1&amp;origin=https%3A%2F%2Fwww.ideo.com&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;enablejsapi=1&amp;widgetid=1"
+                  id="widget2"
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="TextBlock TextBlock--wide">
+        <div class="Grid">
+          <div class="TextBlock-Content">
+            <div class="Text TextStyles">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                odio ligula, ornare non urna id, pellentesque elementum mauris.
+                Vestibulum sapien ipsum, luctus quis semper commodo, pulvinar
+                eget risus. Quisque vel neque libero. Duis posuere est luctus
+                fringilla accumsan. Vestibulum varius bibendum ipsum, nec
+                venenatis velit elementum nec. Nulla tempus diam quis elit
+                tincidunt, quis convallis massa gravida. Morbi sagittis commodo
+                sapien, vel pharetra elit luctus id. Phasellus convallis
+                interdum orci a finibus. Proin maximus ante sit amet ultricies
+                imperdiet. Quisque ac ipsum eros. Morbi pretium porttitor
+                consequat. Sed tempus pellentesque velit non.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="ImageBlock ImageBlock--wide ImageBlockModifyer--center">
+        <div class="ImageBlock-InlineItemsContainer ImageBlock--NoCaption">
+          <div class="ImageBlock-ImageContainer">
+            <div class="SmartImage SmartImage--inline ImageBlock-Image">
+              <video
+                autoplay=""
+                loop=""
+                muted=""
+                playsinline=""
+                class="SmartImage--inline-Image"
+                src="https://churchill-next.imgix.net/images/posts/cd_3.gif?dpr=1&amp;fit=max&amp;fm=mp4&amp;w=500"
+                poster="https://churchill-next.imgix.net/images/posts/cd_3.gif?dpr=1&amp;fit=max&amp;fm=jpg&amp;w=500"
+              ></video>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="TextBlock TextBlock--wide">
+        <div class="Grid">
+          <div class="TextBlock-Content">
+            <div class="Text TextStyles">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse tincidunt dui luctus odio congue vestibulum.
+                Vestibulum volutpat risus vitae tellus varius facilisis. Ut
+                scelerisque est turpis, a euismod enim tincidunt in. Proin
+                pharetra nulla vel mi hendrerit aliquet. Curabitur vitae ex
+                congue, ullamcorper leo tristique, efficitur ligula. Vestibulum
+                molestie ipsum.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="QuoteBlock QuoteBlock--NoImage">
+        <div class="Grid">
+          <div class="QuoteBlock-Content">
+            <div class="QuoteBlock-Text">
+              <span>
+                <p>
+                  <em>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer a convallis ex. Pellentesque convallis arcu id
+                    consequat consequat. Fusce ornare semper egestas. Donec vel
+                    ornare tellus. Cras feugiat tincidunt gravida. Aliquam.
+                  </em>
+                </p>
+              </span>
+            </div>
+            <div class="QuoteBlock-Author">
+              <span>
+                <p>
+                  Hernan Carranza, Chief Innovation Officer,
+                  <a
+                    href="https://www.ideo.com/case-study/launching-an-innovation-lab-to-benefit-peruvians"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    >Intercorp
+                  </a>
+                </p>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="TextBlock TextBlock--wide">
+        <div class="Grid">
+          <div class="TextBlock-Content">
+            <div class="Text TextStyles">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur mollis purus diam, id sagittis leo malesuada sed. Sed
+                quis vulputate justo. Maecenas ipsum nisl, lobortis eget sem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Manifest"
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/ideo";
+</style>
