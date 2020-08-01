5aa55a8ea027cab5d904f6f0599c82257540362d
Rafael Sales <rafael.sales93@hotmail.com>
2020-08-01 01:21:52 -0300

    Update Shop.

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -103,105 +103,122 @@
             </div>
           </div>
         </div>
-        <div class="mt-16">
-          <h3 class="text-gray-600 text-2xl font-medium">More Products</h3>
-          <div
-            class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
+      </div>
+    </main>
+
+    <section class="container mx-auto mt-20 px-16">
+      <h3 class="text-3xl text-center">
+        Comantários de quem já faz parte...
+      </h3>
+      <div
+        class="max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md mx-auto mb-10"
+      >
+        <div class="flex justify-between items-center">
+          <span class="font-light text-gray-600">mar 10, 2019</span>
+          <a
+            class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500"
+            href="#"
+            >Design</a
           >
-            <div
-              class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
-            >
-              <div
-                class="flex items-end justify-end h-56 w-full bg-cover"
-                style="background-image: url('https://images.unsplash.com/photo-1563170351-be82bc888aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80')"
-              >
-                <button
-                  class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-                >
-                  <svg
-                    class="h-5 w-5"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-              <div class="px-5 py-3">
-                <h3 class="text-gray-700 uppercase">Chanel</h3>
-                <span class="text-gray-500 mt-2">$12</span>
-              </div>
-            </div>
-            <div
-              class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
-            >
-              <div
-                class="flex items-end justify-end h-56 w-full bg-cover"
-                style="background-image: url('https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')"
-              >
-                <button
-                  class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-                >
-                  <svg
-                    class="h-5 w-5"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-              <div class="px-5 py-3">
-                <h3 class="text-gray-700 uppercase">Man Mix</h3>
-                <span class="text-gray-500 mt-2">$12</span>
-              </div>
-            </div>
-            <div
-              class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
-            >
-              <div
-                class="flex items-end justify-end h-56 w-full bg-cover"
-                style="background-image: url('https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')"
-              >
-                <button
-                  class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-                >
-                  <svg
-                    class="h-5 w-5"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-              <div class="px-5 py-3">
-                <h3 class="text-gray-700 uppercase">Classic watch</h3>
-                <span class="text-gray-500 mt-2">$12</span>
-              </div>
-            </div>
+        </div>
+        <div class="mt-2">
+          <a class="text-2xl text-gray-700 font-bold hover:text-gray-600" href="#"
+            >Accessibility tools for designers and developers</a
+          >
+          <p class="mt-2 text-gray-600">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
+            expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos
+            enim reprehenderit nisi, accusamus delectus nihil quis facere in modi
+            ratione libero!
+          </p>
+        </div>
+        <div class="flex justify-between items-center mt-4">
+          <a class="text-blue-600 hover:underline" href="#">Read more</a>
+          <div>
+            <a class="flex items-center" href="#">
+              <img
+                class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+                src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80"
+                alt="avatar"
+              />
+              <h1 class="text-gray-700 font-bold">Khatab wedaa</h1>
+            </a>
           </div>
         </div>
       </div>
-    </main>
+      <div
+        class="max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md mx-auto mb-10"
+      >
+        <div class="flex justify-between items-center">
+          <span class="font-light text-gray-600">mar 10, 2019</span>
+          <a
+            class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500"
+            href="#"
+            >Design</a
+          >
+        </div>
+        <div class="mt-2">
+          <a class="text-2xl text-gray-700 font-bold hover:text-gray-600" href="#"
+            >Accessibility tools for designers and developers</a
+          >
+          <p class="mt-2 text-gray-600">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
+            expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos
+            enim reprehenderit nisi, accusamus delectus nihil quis facere in modi
+            ratione libero!
+          </p>
+        </div>
+        <div class="flex justify-between items-center mt-4">
+          <a class="text-blue-600 hover:underline" href="#">Read more</a>
+          <div>
+            <a class="flex items-center" href="#">
+              <img
+                class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+                src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80"
+                alt="avatar"
+              />
+              <h1 class="text-gray-700 font-bold">Khatab wedaa</h1>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        class="max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md mx-auto mb-10"
+      >
+        <div class="flex justify-between items-center">
+          <span class="font-light text-gray-600">mar 10, 2019</span>
+          <a
+            class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500"
+            href="#"
+            >Design</a
+          >
+        </div>
+        <div class="mt-2">
+          <a class="text-2xl text-gray-700 font-bold hover:text-gray-600" href="#"
+            >Accessibility tools for designers and developers</a
+          >
+          <p class="mt-2 text-gray-600">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
+            expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos
+            enim reprehenderit nisi, accusamus delectus nihil quis facere in modi
+            ratione libero!
+          </p>
+        </div>
+        <div class="flex justify-between items-center mt-4">
+          <a class="text-blue-600 hover:underline" href="#">Read more</a>
+          <div>
+            <a class="flex items-center" href="#">
+              <img
+                class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+                src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80"
+                alt="avatar"
+              />
+              <h1 class="text-gray-700 font-bold">Khatab wedaa</h1>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <div class="pagination mt-20 mb-6 px-0 sm:px-2">
       <div class="grid grid-cols-12 gap-2">
